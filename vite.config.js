172113f1da088d839483b0dd35f48f55387b60a9
gdke <gdke@epoint.com.cn>
2025-04-16 10:16:20 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -4,23 +4,7 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
-const config = {
-  plugins: [
-    vue(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-  ],
-  server: {
-    port: 3000,
-    host: true
-  }
-}
-
-export default defineConfig(config)
+export default defineConfig({
   plugins: [
     vue(),
     AutoImport({
